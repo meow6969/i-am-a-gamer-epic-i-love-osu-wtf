d5f23959ad57e9261725d1e42552469d5ded5c98
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-09-16 10:41:54 -0700

    fixed sexism

--- createwithcode/My project 2020/Assets/Scripts/PlayerController.cs
+++ createwithcode/My project 2020/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         
+        // we wont move the vehicle ever it will take too long i will literally collapse and die before i can ever do anything in this class
+        // nvm!!!
+        // this is literally so crazyyyyyyyyyyyyyyyyy it like MOVE WTF
+        transform.Translate(0, 0, 1);
+        transofrm.Translate(Vector3);
     }
 }
