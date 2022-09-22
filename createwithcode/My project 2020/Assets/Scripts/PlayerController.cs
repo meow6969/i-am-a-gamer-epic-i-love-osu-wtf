c858727cc8cf75817a0d66c07af6e1cab1807bbc
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-09-22 10:38:38 -0700

    solved poverty

--- createwithcode/My project 2020/Assets/Scripts/PlayerController.cs
+++ createwithcode/My project 2020/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,6 @@ public class PlayerController : MonoBehaviour
         // nvm!!!
         // this is literally so crazyyyyyyyyyyyyyyyyy it like MOVE WTF
         // transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
