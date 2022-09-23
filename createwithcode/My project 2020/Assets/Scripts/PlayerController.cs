0e1b607815bb79791b65cbe309c3cfa3f04a025d
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-09-23 10:38:47 -0700

    got minecraft blocked fuck you im not solving anything tday

--- createwithcode/My project 2020/Assets/Scripts/PlayerController.cs
+++ createwithcode/My project 2020/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //bruh i dun got teh minecraft gone :sob: i am literally crying and shaking and crying and like wtf now i need to set up a proxy to get around this
+        //which is going to be annoying im literally going to be bored as shit the entire FUCKING PERIOD OH MY GOD WHY ms hansen i am going to kill
+        //you you litearlly RUINED minecraft FOR AN ENTIER PERIOD! I CANT EVEN FUCKKKING GET AROUND IT SINCE I CANT SET UP A PROXY ON MY FONE
+        //WHY DID YOU DO THIS MIS HANSIN WHY DO U THINK BLOCKING MINE CRAF T WILL STOP ME>>>>???????? i am more pwoerful than you can possibry 
+        //imagine you dont even KNOW like what thed fuck wer you thinking i am litearlly the smartest human being alive and am better then 
+        //everyone else fr and you just DELETE MY FUCKING MINECRAFT?! FUCK YOU FUCK YOU FUCK YOU I HATE YOU!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //now i have to listen to this dumb ass bitch asian say a bunch of dumb shit i dont care anbout i want to jill him too!! i am sucha  
+        //gangster 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        horizontalInput = Input.GetAxis("Horizontal");
         
         // we wont move the vehicle ever it will take too long i will literally collapse and die before i can ever do anything in this class
         // nvm!!!
         // this is literally so crazyyyyyyyyyyyyyyyyy it like MOVE WTF
         // transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
