7aeb1dca8bef21656f07f01a8d8b7c3d254d1bc7
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-09-28 10:08:54 -0700

    fix death threats

--- createwithcode/My project 2020/Assets/Scripts/PlayerController.cs
+++ createwithcode/My project 2020/Assets/Scripts/PlayerController.cs
@@ -14,13 +14,12 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         //bruh i dun got teh minecraft gone :sob: i am literally crying and shaking and crying and like wtf now i need to set up a proxy to get around this
-        //which is going to be annoying im literally going to be bored as shit the entire FUCKING PERIOD OH MY GOD WHY ms hansen i am going to kill
-        //you you litearlly RUINED minecraft FOR AN ENTIER PERIOD! I CANT EVEN FUCKKKING GET AROUND IT SINCE I CANT SET UP A PROXY ON MY FONE
+        //which is going to be annoying im literally going to be bored the entire PERIOD OH MY GOD WHY ms hansen 
+        //you litearlly RUINED minecraft FOR AN ENTIER PERIOD! I CANT EVEN GET AROUND IT SINCE I CANT SET UP A PROXY ON MY FONE
         //WHY DID YOU DO THIS MIS HANSIN WHY DO U THINK BLOCKING MINE CRAF T WILL STOP ME>>>>???????? i am more pwoerful than you can possibry 
-        //imagine you dont even KNOW like what thed fuck wer you thinking i am litearlly the smartest human being alive and am better then 
-        //everyone else fr and you just DELETE MY FUCKING MINECRAFT?! FUCK YOU FUCK YOU FUCK YOU I HATE YOU!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        //now i have to listen to this dumb ass bitch asian say a bunch of dumb shit i dont care anbout i want to jill him too!! i am sucha  
-        //gangster 
+        //imagine you dont even KNOW like what wer you thinking i am litearlly the smartest human being alive and am better then 
+        //everyone else fr and you just DELETE MY MINECRAFT?!
+        //now i have to listen to this guy say a bunch of dumb things i dont care anbout!! i am sucha  gangster
     }
 
     // Update is called once per frame
@@ -36,7 +35,7 @@ public class PlayerController : MonoBehaviour
         // transform.Translate(0, 0, 1);
         //vehicle go croom veoom here CRAZY
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        //veuhile go turny
+        //veuhile go turny LITERALLY SO CRAZY
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
         //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
