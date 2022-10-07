89585e1afdc8e92f791428ed65739c5f0a156c90
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-10-07 10:04:10 -0700

    solve climate change

---
+++ createwithcode/teh second 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * offset);
+        if (transform.position[0] < -15) {
+            transform.Translate(Vector3.right * offset * -1);
+        }
+    }
+}
