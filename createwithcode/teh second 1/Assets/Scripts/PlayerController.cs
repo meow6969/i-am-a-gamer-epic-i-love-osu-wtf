e4291ed37687b51d6692d98800126aed1c72cde3
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-10-12 10:43:10 -0700

    SOLVED LAIN

--- createwithcode/teh second 1/Assets/Scripts/PlayerController.cs
+++ createwithcode/teh second 1/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 16.0f;
+    public GameObject projectilePrefab; 
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,9 @@ public class PlayerController : MonoBehaviour
         }
         else if (transform.position.x > xRange) {
            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X)) {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
     
