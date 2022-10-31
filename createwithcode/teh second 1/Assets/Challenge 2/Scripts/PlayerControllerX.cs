ae4423d22652569a0342e47d5d2d19033b755881
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-10-31 10:16:08 -0700

    solved google doodle game

---
+++ createwithcode/teh second 1/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float timer;
+    private float spawnLimit = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        // On spacebar press, send dog
+        //Debug.Log(timer);
+        if (Input.GetKeyDown(KeyCode.Space) && timer > spawnLimit)
+        {
+            timer = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
