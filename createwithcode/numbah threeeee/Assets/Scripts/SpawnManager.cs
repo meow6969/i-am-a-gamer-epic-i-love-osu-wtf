0dd154d40e0c5a63cd72939368dee1936011b986
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-11-28 10:19:27 -0800

    lets all love lain!

--- createwithcode/numbah threeeee/Assets/Scripts/SpawnManager.cs
+++ createwithcode/numbah threeeee/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,22 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("IAmAddictedToHeroin", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void IAmAddictedToHeroin() { // please take me out of school so then i can inject more heroin into my body i hate everything please i want this to end
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
