ddc2fe353135357afe289c2e5f41e556d9232545
meow6969 <sdfsdfaasdfdcf@gmail.com>
2022-12-02 10:42:02 -0800

    solved konata breakcore

--- createwithcode/numbah threeeee/Assets/Scripts/MoveLeft.cs
+++ createwithcode/numbah threeeee/Assets/Scripts/MoveLeft.cs
@@ -6,6 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 15.0f;
     private PlayerController playerControllerScript; 
+    private float downBound = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,9 @@ public class MoveLeft : MonoBehaviour
     {
         if (!playerControllerScript.gameOver) {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (transform.position.y < downBound) {
+            Destroy(gameObject);
         }
         
     }
--- createwithcode/numbah threeeee/Assets/Scripts/SpawnManager.cs
+++ createwithcode/numbah threeeee/Assets/Scripts/SpawnManager.cs
@@ -8,21 +8,25 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("IAmAddictedToHeroin", startDelay, spawnInterval);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        // Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        // Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 
     void IAmAddictedToHeroin() { // please take me out of school so then i can inject more heroin into my body i hate everything please i want this to end
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (!playerControllerScript.gameOver) {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
