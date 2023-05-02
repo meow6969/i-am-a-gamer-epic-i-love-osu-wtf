edd677d45ac50efbe0d16f643f38dfc43b1d93ed
meow6969 <sdfsdfaasdfdcf@gmail.com>
2023-05-02 10:41:43 -0700

    aaheheahehahahehaehaeahaaahahahhahhahaha

--- createwithcode/デレクゲイ/Assets/Scripts/Rocket.cs
+++ createwithcode/デレクゲイ/Assets/Scripts/Rocket.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangerEnemy : MonoBehaviour
+public class Rocket : MonoBehaviour
 {
-    private float farthestAway = 5.0f;
-    public float closestAway = 25.0f;
-    private float speed = 7.0f;
+    private float speed = 14.0f;
+    [SerializeField] bool moving = true;
 
     private GameObject player;
     private Rigidbody selfRb;
@@ -21,9 +20,7 @@ public class RangerEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, player.transform.position);
-
-        if (distance > farthestAway) {
+        if (moving) {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
 
@@ -32,5 +29,20 @@ public class RangerEnemy : MonoBehaviour
         float angle = -Mathf.Atan2(v,h) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(180, angle, 0);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        // Debug.Log(collision.collider.name);
+        if (collision.collider.name == "Player") {
+            // Debug.Log(collision.collider.name);
+            moving = false;
+        }
+    }
+
+    void OnCollisionExit(Collision other) {
+        if (other.collider.name == "Player") {
+            // Debug.Log(collision.collider.name);
+            moving = true;
+        }
     }
 }
