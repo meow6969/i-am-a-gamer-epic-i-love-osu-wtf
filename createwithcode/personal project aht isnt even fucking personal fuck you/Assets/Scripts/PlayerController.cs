fe6bf4cafcc7a89b973c06f69f26cd7ca7df0d6a
meow6969 <sdfsdfaasdfdcf@gmail.com>
2023-05-16 00:44:24 -0700

    AAAAAAAAAAAAAAAAAAAAAAAA

--- createwithcode/personal project aht isnt even fucking personal fuck you/Assets/Scripts/PlayerController.cs
+++ createwithcode/personal project aht isnt even fucking personal fuck you/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 50.0f;
+    private float speed = 100.0f;
     private float zBound = 6;
     private Rigidbody playerRb;
 
